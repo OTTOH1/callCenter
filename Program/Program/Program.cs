c0d50981efd4a67da1a3f9b71e21f04e3db38890
Anton <vafon2@gmail.com>
2014-12-27 13:03:16 +0200

    Add class for Call center.

--- Program/Program/Program.cs
+++ Program/Program/Program.cs
@@ -34,4 +34,61 @@ namespace Program
     {
 
     }
+
+    class CallCenter
+    {
+        private List<Employee> staff = new List<Employee>();
+        public void Add(Employee emp)
+        {
+            staff.Add(emp);
+        }
+        public Employee getCallHandler()
+        {
+            // Going through every employee and make a list of free operators.
+            List<Employee> FreeOperators = new List<Employee>();
+            List<Employee> FreeTM = new List<Employee>();
+            List<Employee> FreeM = new List<Employee>();
+
+            for (int i = 0; i < staff.Count; i++)
+            {
+                if (staff[i].Free == true)
+                {
+                    if (staff[i].GetType() == typeof(Operator))
+                    {
+                        FreeOperators.Add(staff[i]);
+                    }
+                    else if (staff[i].GetType() == typeof(TM))
+                    {
+                        FreeTM.Add(staff[i]);
+                    }
+                    else if (staff[i].GetType() == typeof(M))
+                    {
+                        FreeM.Add(staff[i]);
+                    } 
+                }
+            }
+
+            Random rnd = new Random();
+
+            if (FreeOperators.Count > 0)
+            {
+                int index = rnd.Next(FreeOperators.Count);
+                return FreeOperators[index];
+            }
+            else if (FreeTM.Count > 0)
+            {
+                int index = rnd.Next(FreeTM.Count);
+                return FreeTM[index];
+            }
+            else if (FreeM.Count > 0)
+            {
+                int index = rnd.Next(FreeM.Count);
+                return FreeM[index];
+            }
+            else
+            {
+                throw new Exception("There is no free operators in the call center.");
+            }
+        }
+    }
 }
